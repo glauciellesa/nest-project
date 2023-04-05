d4838ac1f087b2764ec4ccaa11ac00f3e3e7fd88
Glaucielle Sá <glauciellesa@gmail.com>
2023-04-05 15:25:58 +0200

    Add method post and delete

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,12 +1,15 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
   HttpException,
   HttpStatus,
   Param,
   ParseIntPipe,
+  Post,
 } from '@nestjs/common';
-import { CourseNotFound } from './courses.model';
+import { Course, CourseNotFound } from './courses.model';
 import { CoursesService } from './courses.service';
 
 @Controller('courses')
@@ -16,7 +19,8 @@ export class CoursesController {
     this.coursesService = coursesService;
   } /* constructor(private readonly coursesService: CoursesService){}   Dependency Injection  */
 
-  @Get() /* Controle method  that there is just a defaut route implemented*/
+  @Get() /* Controle method  that there is just a defaut route implemented.
+   @Get method tells Nest to create a handler for a specific endpoint for HTTP requests.*/
   async getAllCourses() {
     const courses = await this.coursesService.getCourses();
     return courses;
@@ -35,5 +39,16 @@ export class CoursesController {
         throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
       }
     }
+  }
+
+  @Post()
+  async addCourse(@Body() createCourseDto: Course) {
+    const course = await this.coursesService.addCourse(createCourseDto);
+    return course;
+  }
+
+  @Delete(':id')
+  async deleteCourse(@Param('id', ParseIntPipe) id: number) {
+    const courses = await this.coursesService.deleteCourse(id);
   }
 }
--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,5 @@
 import { HttpException, Injectable } from '@nestjs/common';
+import { resolve } from 'path';
 import { COURSES } from './courses.mock';
 import { Course, CourseNotFound } from './courses.model';
 
@@ -21,5 +22,32 @@ export class CoursesService {
       }
       resolve(course);
     });
+  }
+
+  addCourse(course: Course): Promise<Course[]> {
+    return new Promise((resolve) => {
+      this.courses.push(course);
+      resolve(this.courses);
+    });
+  }
+
+  deleteCourse(courseId: number): Promise<Course[]> {
+    return new Promise((resolve) => {
+      let course = this.courses.find((course) => course.id === courseId);
+      if (!course) {
+        throw new CourseNotFound();
+      }
+      this.courses.splice(this.courses.indexOf(course), 1);
+      resolve(this.courses);
+    });
+
+    /*    return new Promise((resolve) => {
+      let index = this.courses.findIndex((course) => course.id === courseId);
+      if (index === -1) {
+        throw new CourseNotFound();
+      }
+      this.courses.splice(index, 1);
+      resolve(this.courses);
+    }); */
   }
 }
